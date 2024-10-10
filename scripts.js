d84c76723d80b52d8455e09f54000090fa508ab8
bearmind-mykhailo <mykhailo@bearmind.tech>
2024-10-10 19:52:41 +0400

    refactor: divide scripts.js

--- scripts.js
+++ scripts.js
@@ -28,85 +28,6 @@ const connectNewNetworkSSIDSelectEl = document.getElementById('connect-new-netwo
 const connectNewNetworkFormEl = document.getElementById('connect-new-network-form');
 const connectNewNetworkPasswordEl = document.getElementById('connect-new-network-password');
 
-// API CALLS
-async function fetchWithErrorHandling(url, options) {
-  const response = await fetch(url, options);
-
-  // not in 200-299 range
-  if (!response.ok) {
-    throw new Error(`HTTP error! At: ${url} Status: ${response.status}`);
-  }
-
-  return response;
-}
-
-async function connectNewNetwork(network, psk) {
-  await fetchWithErrorHandling('/connect_wifi/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      ssid: network,
-      psk: psk,
-    }),
-  });
-}
-
-async function saveNewNetwork(network, psk) {
-  await fetchWithErrorHandling('/save_wifi/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      ssid: network,
-      psk: psk,
-    }),
-  });
-}
-
-async function deleteKnownNetwork(network) {
-  await fetchWithErrorHandling('/delete_wifi/', {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      ssid: network,
-    }),
-  });
-}
-
-async function disconnectDockstation() {
-  await fetchWithErrorHandling('/cancel/', {
-    method: 'POST',
-  });
-}
-
-async function refreshNetworks() {
-  let networks = [];
-  let known_networks = [];
-
-  try {
-    const [scanResponse, knownResponse] = await Promise.all([
-      fetchWithErrorHandling('/scan_wifi/'),
-      fetchWithErrorHandling('/known_wifi/'),
-    ]);
-
-    const scanData = await scanResponse.json();
-    const knownData = await knownResponse.json();
-
-    networks.push(...scanData.networks);
-    known_networks.push(...knownData.known_networks);
-
-    insertNetworksInList(networks, 'connect-new-network-ssid-select');
-    createNetworkCard(known_networks, 'known-network-list');
-  } catch (error) {
-    console.error('Error fetching WiFi networks', error);
-  }
-}
-
 // PROMPT ACTIONS
 confirmDialogEl.addEventListener('close', async () => {
   if (confirmDialogEl.returnValue === 'confirm') {
@@ -130,73 +51,24 @@ confirmDialogEl.addEventListener('close', async () => {
       }
     } else if (disconnect) {
       // DISCONNECT NOW
-      tabsContentEl.style.display = 'none';
-      containerContentEl.style.maxHeight = '28rem';
-      confirmationHeaderEl.textContent = 'Disconnecting';
-      confirmationInfo1El.textContent =
-        'Your computer will now be disconnected from the Bearmind Dockstation.';
-      confirmationIconEl.src = './static/icons/disconnect.svg';
-      confirmationContentEl.style.display = 'flex';
-      delete confirmDialogEl.dataset.disconnect;
+      try {
+        await disconnectDockstation();
+  
+        tabsContentEl.style.display = 'none';
+        containerContentEl.style.maxHeight = '28rem';
+        confirmationHeaderEl.textContent = 'Disconnecting';
+        confirmationInfo1El.textContent =
+          'Your computer will now be disconnected from the Bearmind Dockstation.';
+        confirmationIconEl.src = './static/icons/disconnect.svg';
+        confirmationContentEl.style.display = 'flex';
+        delete confirmDialogEl.dataset.disconnect;
+      } catch (error) {
+        console.error('onDisconnect - error: ', error);
+        notyf.error('Failed to disconnect dockstation!');
+      }
     }
   }
 });
-
-function insertNetworksInList(networks, id) {
-  const ssidSelectEl = document.getElementById(id);
-  ssidSelectEl.innerHTML = '';
-
-  const defaultOption = document.createElement('option');
-  defaultOption.value = '';
-  defaultOption.disabled = true;
-  defaultOption.hidden = true;
-  defaultOption.textContent = 'Select SSID';
-  ssidSelectEl.appendChild(defaultOption);
-
-  networks.forEach((network) => {
-    const option = document.createElement('option');
-    option.value = network;
-    option.textContent = network;
-    ssidSelectEl.appendChild(option);
-  });
-
-  ssidSelectEl.value = '';
-}
-
-function createNetworkCard(networks, id) {
-  const networksListEl = document.getElementById(id);
-  networksListEl.innerHTML = '';
-
-  networks.forEach((network, index) => {
-    const listItem = document.createElement('li');
-    listItem.id = `${network}-${index}`;
-    listItem.textContent = network;
-
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = '×';
-    deleteButton.classList.add('del-network-btn');
-
-    // show modal & modify text
-    deleteButton.addEventListener('click', (event) => {
-      event.preventDefault();
-
-      confirmDialogTextEl.textContent = 'Do you really want to remove ';
-      const strongEl = document.createElement('strong');
-      strongEl.textContent = network;
-      confirmDialogTextEl.appendChild(strongEl);
-      confirmDialogTextEl.appendChild(document.createTextNode('?'));
-
-      confirmDialogEl.dataset.selectedNetwork = network;
-      confirmDialogEl.dataset.selectedNetworkIndex = index;
-      confirmDeleteDialogHeaderEl.textContent = 'Delete network';
-
-      confirmDialogEl.showModal();
-    });
-
-    listItem.appendChild(deleteButton);
-    networksListEl.appendChild(listItem);
-  });
-}
 
 function testOnDelete() {
   confirmDialogTextEl.style.display = 'block';
@@ -210,19 +82,12 @@ function testOnDelete() {
   confirmDialogEl.showModal();
 }
 
-async function onDisconnect() {
-  try {
-    await disconnectDockstation();
-
+function onDisconnect() {
     confirmDeleteDialogHeaderEl.textContent = 'Disconnect now?';
     confirmDialogTextEl.style.display = 'none';
     confirmDialogTextEl.textContent = '';
     confirmDialogEl.dataset.disconnect = true;
     confirmDialogEl.showModal();
-  } catch (error) {
-    console.error('onDisconnect - error: ', error);
-    notyf.error('Failed to disconnect dockstation!');
-  }
 }
 
 function showTab(tabId) {
@@ -255,66 +120,6 @@ async function goBack() {
   await refreshNetworks();
   showTab('connect-new-network-form');
 }
-
-// CONNECT NEW NETWORK FORM
-connectNewNetworkFormEl.addEventListener('submit', async function (event) {
-  event.preventDefault();
-
-  try {
-    await connectNewNetwork(connectNewNetworkSSIDSelectEl.value, connectNewNetworkPasswordEl.value);
-    tabsContentEl.style.display = 'none';
-    containerContentEl.style.maxHeight = '28rem';
-
-    // set custom texts
-    confirmationInfo1El.textContent = 'The Bearmind Dockstation is now connecting to ';
-    const strongEl = document.createElement('strong');
-    strongEl.textContent = connectNewNetworkSSIDSelectEl.value;
-    confirmationInfo1El.appendChild(strongEl);
-    confirmationInfo1El.appendChild(
-      document.createTextNode('. Your computer will be disconnected from the local network.'),
-    );
-
-    // show confirmation content
-    confirmationContentEl.style.display = 'flex';
-    confirmationReturnBtnEl.style.display = 'none';
-    confirmationInfo2El.style.display = 'block';
-    confirmationInfo2El.textContent =
-      'Monitor the Wi-Fi LED on the Bearmind Dockstation to ensure that it has successfully connected to the Wi-Fi network.';
-
-    notyf.success('Successfully connected to the new Wi-Fi network!');
-  } catch (error) {
-    console.error('connectNewNetwork - error: ', error);
-    notyf.error('Error connecting to the new Wi-Fi network!');
-  }
-});
-
-// SAVE NEW NETWORK FORM
-saveNewNetworkFormEl.addEventListener('submit', async function (event) {
-  event.preventDefault();
-
-  try {
-    await saveNewNetwork(saveNewSSIDInputEl.value, saveNewNetworkPasswordEl.value);
-
-    tabsContentEl.style.display = 'none';
-    containerContentEl.style.maxHeight = '28rem';
-
-    // set custom texts
-    confirmationHeaderEl.textContent = 'A new Wi-Fi network has been added!';
-    confirmationInfo1El.textContent =
-      'The network has been successfully saved. It will automatically connect whenever you are within range, ensuring a seamless connection.';
-    confirmationInfo2El.style.display = 'none';
-    confirmationInfo2El.style.textContent = '';
-
-    // show confirmation content
-    confirmationContentEl.style.display = 'flex';
-    confirmationReturnBtnEl.style.display = 'block';
-
-    notyf.success('Successfully saved a new Wi-Fi network!');
-  } catch (error) {
-    console.error('saveNewNetwork - error: ', error);
-    notyf.error('Failed to save a new Wi-Fi network!');
-  }
-});
 
 // ON DOM LOAD
 document.addEventListener('DOMContentLoaded', async () => {
