ac3ea8dbac599bbd8c500fb96d5ad7608eac6d18
bearmind-mykhailo <mykhailo@bearmind.tech>
2024-10-10 18:31:45 +0400

    feat: refactor & add post requests

--- scripts.js
+++ scripts.js
@@ -1,27 +1,98 @@
+// INIT LIBS
 var notyf = new Notyf({
   position: { x: 'right', y: 'top' },
   ripple: false,
 });
 
+// DOM NODES
 const tabsContentEl = document.getElementById('tabs-content');
+
 const containerContentEl = document.getElementById('container-content');
+
 const confirmationContentEl = document.getElementById('confirmation-content');
 const confirmationHeaderEl = document.getElementById('confirmation-header');
 const confirmationIconEl = document.getElementById('confirmation-icon');
 const confirmationInfo1El = document.getElementById('confirmation-info-1');
 const confirmationInfo2El = document.getElementById('confirmation-info-2');
-const connectNewNetworkSSIDSelect = document.getElementById('connect-new-network-ssid-select');
 const confirmationReturnBtnEl = document.getElementById('confirmation-return-btn');
-const saveNewSSIDInput = document.getElementById('save-new-ssid-input');
-const saveNewNetworkPassword = document.getElementById('save-new-network-password');
+
+const saveNewSSIDInputEl = document.getElementById('save-new-ssid-input');
+const saveNewNetworkPasswordEl = document.getElementById('save-new-network-password');
+const saveNewNetworkFormEl = document.getElementById('save-new-network-form');
+
 const confirmDialogEl = document.getElementById('confirm-dialog');
 const confirmDeleteDialogHeaderEl = document.getElementById('confirm-dialog-header');
-const connectNewNetworkForm = document.getElementById('connect-new-network-form');
-const saveNewNetworkForm = document.getElementById('save-new-network-form');
 const confirmDialogTextEl = document.getElementById('confirm-dialog-text');
 
+const connectNewNetworkSSIDSelectEl = document.getElementById('connect-new-network-ssid-select');
+const connectNewNetworkFormEl = document.getElementById('connect-new-network-form');
+const connectNewNetworkPasswordEl = document.getElementById('connect-new-network-password');
+
+// API CALLS
+function connectNewNetwork(network, psk) {
+  fetch('/connect_wifi/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      ssid: network,
+      psk: psk,
+    }),
+  });
+}
+function saveNewNetwork(network, psk) {
+  fetch('/save_wifi/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      ssid: network,
+      psk: psk,
+    }),
+  });
+}
+
+function deleteKnownNetwork(network) {
+  fetch('/delete_wifi/', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      ssid: network,
+    }),
+  });
+}
+
+function refreshNetworks() {
+  let networks = [];
+  let known_networks = [];
+
+  Promise.all([
+    fetch('/scan_wifi/')
+      .then((response) => response.json())
+      .then((data) => {
+        networks.push(...data.networks);
+      }),
+    fetch('/known_wifi/')
+      .then((response) => response.json())
+      .then((data) => {
+        known_networks.push(...data.known_networks);
+      }),
+  ])
+    .then(() => {
+      insertNetworksInList(networks, 'connect-new-network-ssid-select');
+      createNetworkCard(known_networks, 'known-network-list');
+    })
+    .catch((error) => {
+      console.error('Error fetching WiFi networks', error);
+    });
+}
+
+// PROMPT ACTIONS
 confirmDialogEl.addEventListener('close', () => {
-  // DIALOG CONFIRM ACTIONS
   if (confirmDialogEl.returnValue === 'confirm') {
     const selectedNetwork = confirmDialogEl.dataset.selectedNetwork;
     const selectedNetworkIndex = confirmDialogEl.dataset.selectedNetworkIndex;
@@ -127,55 +198,6 @@ function onDisconnect() {
   confirmDialogEl.showModal();
 }
 
-function fetchKnownNetworks() {
-  fetch('/known_wifi/')
-    .then((response) => response.json())
-    .then((data) => createNetworkCard(data.known_networks, 'known-network-list'));
-}
-
-function fetchNetworks() {
-  fetch('/scan_wifi/')
-    .then((response) => response.json())
-    .then((data) => insertNetworksInList(data.networks, 'connect-new-network-ssid-select'));
-}
-
-function deleteKnownNetwork(network) {
-  fetch('/delete_wifi/', {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      ssid: network,
-    }),
-  });
-}
-
-function refreshNetworks() {
-  let networks = [];
-  let known_networks = [];
-
-  Promise.all([
-    fetch('/scan_wifi/')
-      .then((response) => response.json())
-      .then((data) => {
-        networks.push(...data.networks);
-      }),
-    fetch('/known_wifi/')
-      .then((response) => response.json())
-      .then((data) => {
-        known_networks.push(...data.known_networks);
-      }),
-  ])
-    .then(() => {
-      insertNetworksInList(networks, 'connect-new-network-ssid-select');
-      createNetworkCard(known_networks, 'known-network-list');
-    })
-    .catch((error) => {
-      console.error('Error fetching WiFi networks', error);
-    });
-}
-
 function showTab(tabId) {
   const tabs = document.querySelectorAll('.tab');
   tabs.forEach((tab) => {
@@ -192,11 +214,7 @@ function showTab(tabId) {
   document.getElementById(`header-${tabId}`).classList.add('header__item__active');
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  refreshNetworks();
-  showTab('connect-new-network-form');
-});
-
+// RETURN FROM SAVE WIFI CONFIRMATION VIEW
 function goBack() {
   confirmationContentEl.style.display = 'none';
   confirmationReturnBtnEl.style.display = 'none';
@@ -204,56 +222,76 @@ function goBack() {
   tabsContentEl.style.display = 'flex';
   containerContentEl.style.maxHeight = '52rem';
 
-  saveNewSSIDInput.value = '';
-  saveNewNetworkPassword.value = '';
+  saveNewSSIDInputEl.value = '';
+  saveNewNetworkPasswordEl.value = '';
 
   refreshNetworks();
   showTab('connect-new-network-form');
 }
 
-connectNewNetworkForm.addEventListener('submit', function (event) {
-  // TODO - api call
+// CONNECT NEW NETWORK FORM
+connectNewNetworkFormEl.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  tabsContentEl.style.display = 'none';
-  containerContentEl.style.maxHeight = '28rem';
+  try {
+    connectNewNetwork(connectNewNetworkSSIDSelectEl.value, connectNewNetworkPasswordEl.value);
 
-  // set custom texts
-  confirmationInfo1El.textContent = 'The Bearmind Dockstation is now connecting to ';
-  const strongEl = document.createElement('strong');
-  strongEl.textContent = connectNewNetworkSSIDSelect.value;
-  confirmationInfo1El.appendChild(strongEl);
-  confirmationInfo1El.appendChild(
-    document.createTextNode('. Your computer will be disconnected from the local network.'),
-  );
+    tabsContentEl.style.display = 'none';
+    containerContentEl.style.maxHeight = '28rem';
 
-  // show confirmation content
-  confirmationContentEl.style.display = 'flex';
-  confirmationReturnBtnEl.style.display = 'none';
-  confirmationInfo2El.style.display = 'block';
-  confirmationInfo2El.textContent =
-    'Monitor the Wi-Fi LED on the Bearmind Dockstation to ensure that it has successfully connected to the Wi-Fi network.';
+    // set custom texts
+    confirmationInfo1El.textContent = 'The Bearmind Dockstation is now connecting to ';
+    const strongEl = document.createElement('strong');
+    strongEl.textContent = connectNewNetworkSSIDSelectEl.value;
+    confirmationInfo1El.appendChild(strongEl);
+    confirmationInfo1El.appendChild(
+      document.createTextNode('. Your computer will be disconnected from the local network.'),
+    );
 
-  notyf.success('Successfully connected to the new Wi-Fi network!');
+    // show confirmation content
+    confirmationContentEl.style.display = 'flex';
+    confirmationReturnBtnEl.style.display = 'none';
+    confirmationInfo2El.style.display = 'block';
+    confirmationInfo2El.textContent =
+      'Monitor the Wi-Fi LED on the Bearmind Dockstation to ensure that it has successfully connected to the Wi-Fi network.';
+
+    notyf.success('Successfully connected to the new Wi-Fi network!');
+  } catch (error) {
+    console.error('connectNewNetwork - error: ', error);
+    notyf.error('Error connecting to the new Wi-Fi network!');
+  }
 });
 
-saveNewNetworkForm.addEventListener('submit', function (event) {
-  // TODO - api call
+// SAVE NEW NETWORK FORM
+saveNewNetworkFormEl.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  tabsContentEl.style.display = 'none';
-  containerContentEl.style.maxHeight = '28rem';
+  try {
+    saveNewNetwork(saveNewSSIDInputEl.value, saveNewNetworkPasswordEl.value);
 
-  // set custom texts
-  confirmationHeaderEl.textContent = 'A new Wi-Fi network has been added!';
-  confirmationInfo1El.textContent =
-    'The network has been successfully saved. It will automatically connect whenever you are within range, ensuring a seamless connection.';
-  confirmationInfo2El.style.display = 'none';
-  confirmationInfo2El.style.textContent = '';
+    tabsContentEl.style.display = 'none';
+    containerContentEl.style.maxHeight = '28rem';
 
-  // show confirmation content
-  confirmationContentEl.style.display = 'flex';
-  confirmationReturnBtnEl.style.display = 'block';
+    // set custom texts
+    confirmationHeaderEl.textContent = 'A new Wi-Fi network has been added!';
+    confirmationInfo1El.textContent =
+      'The network has been successfully saved. It will automatically connect whenever you are within range, ensuring a seamless connection.';
+    confirmationInfo2El.style.display = 'none';
+    confirmationInfo2El.style.textContent = '';
 
-  notyf.success('Successfully saved a new Wi-Fi network!');
+    // show confirmation content
+    confirmationContentEl.style.display = 'flex';
+    confirmationReturnBtnEl.style.display = 'block';
+
+    notyf.success('Successfully saved a new Wi-Fi network!');
+  } catch (error) {
+    console.error('saveNewNetwork - error: ', error);
+    notyf.error('Failed to save a new Wi-Fi network!');
+  }
+});
+
+// ON DOM LOAD
+document.addEventListener('DOMContentLoaded', () => {
+  refreshNetworks();
+  showTab('connect-new-network-form');
 });
